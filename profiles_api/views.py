ac8213425e12d320c3cfc08fbc028ea58e9e60b2
italoScript <italoyoupw@gmail.com>
2021-08-22 03:21:33 -0300

    feed users finished

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
-from profiles_api import serializers
-
+from profiles_api import permissions, serializers
+from profiles_api import models
 
 class HelloApiView(APIView):
     """Test api view"""
@@ -35,3 +41,42 @@ class HelloApiView(APIView):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
              )
+
+    def put(self,request, pk=None):
+
+        return Response({'method':'PUT'})
+
+    def patch(self,request, pk=None):
+
+        return Response({'method':'PATCH'})
+    
+    def delete(self,request, pk=None):
+
+        return Response({'method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentications tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
